1d72224da1a16e825b8efe40213ee1c1627c8da1
Sumit <sumitvishwakarma207989@gmail.com>
2025-10-29 23:44:42 +0530

    wrote the main code

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/sumit40000/book-management-api/internal/config"
+	"github.com/sumit40000/book-management-api/internal/routes"
 )
+
+func startServer(){
+	r:= routes.CreateRoutes()
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), r))
+}
 
 func main() {
 	fmt.Println("welcome to the book management api")
+	err:= godotenv.Load()
+	if err!= nil{
+		log.Fatal("failed starting , error : error to load the env file", err)
+	}
 
 	// connecting the database
 	config.ConnectDb()
+
+	// starting the server
+	fmt.Println("Starting the server")
+	fmt.Println("server started successfully, listening on port 8000")
+	startServer()
 }
