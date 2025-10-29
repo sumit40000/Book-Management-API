55c843aba91b23ebbf46e77d87b47921e36224e1
Sumit <sumitvishwakarma207989@gmail.com>
2025-10-29 21:05:14 +0530

    initial commit with basic project folder structure

---
+++ internal/models/models.go
@@ -0,0 +1,12 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Book struct {
+	Id primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Title string `json:"title"`
+	Author string `json:"author"`
+	Price float64 `json:"price"`
+	Rating float64 `json:"rating"`
+}
+
